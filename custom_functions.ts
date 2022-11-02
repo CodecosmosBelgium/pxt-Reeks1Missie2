cd52b99bba90e1e998cb78db5dc7a58debd58287
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2022-11-02 10:54:07 +0100

    startingPosistion

---
+++ custom_functions.ts
@@ -0,0 +1,9 @@
+//% color=190 weight=100 block="CodeCosmos"
+namespace CodeCosmos {
+    //% block.loc.nl="zetKlaar"
+    export function startingPosition() {
+        player.execute("function levels/startingPosition");
+    }
+
+    
+}
