af17528e37a02bf7bafb4ba51f50df19d4e89f94
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-06 16:06:42 +0100

    pxt.json, custom_functions.ts bijwerken

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,5 +1,22 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
+    let correctPositions: Position[] = [];
+
+    function placeAndCheck() {
+        const detectPostion = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
+
+        if (correctPositions.indexOf(detectPostion) === -1 && (blocks.testForBlock(Block.LightGrayConcrete, detectPostion) || blocks.testForBlock(Block.WhiteConcrete, detectPostion) || blocks.testForBlock(Block.RedstoneBlock, detectPostion))) {
+            agent.place(DOWN)
+            correctPositions.push(detectPostion);
+            player.execute(`execute @c ~ ~ ~ particle rwm:checkmark ~ ~1 ~`)
+
+        } else {
+            player.execute(`execute @c ~ ~ ~ particle rwm:barrier ~ ~1 ~`)
+        }
+
+        player.execute(`scoreboard players set @a level_check ${correctPositions.length}`)
+    }
+
     //% block="zetKlaar"
     export function startingPosition() {
         agent.setSlot(1);
