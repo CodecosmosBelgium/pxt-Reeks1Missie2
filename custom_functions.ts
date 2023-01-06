2b3b5aaa8c2d6f980f6d5bac5d0dc2a9208ec1c4
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-06 10:19:29 +0100

    Update main.blocks, custom_functions.ts, t_level10.md, t_level1-6.md

--- custom_functions.ts
+++ custom_functions.ts
@@ -4,16 +4,16 @@ namespace CodeCosmos {
     //% block.loc.nl="zetKlaar"
     export function startingPosition() {
         agent.setSlot(1);
+        agent.setAssist(PLACE_ON_MOVE, true);
+        agent.setItem(REDSTONE_WIRE, 64, 1);
         player.execute("function levels/startingPosition");
     }
 
-    //% block="placeRedstone"
-    //% block.loc.nl="plaats redstone"
-    export function placeRedstone() {
+    //% block="giveRedstone"
+    //% block.loc.nl="geefRedstone"
+    export function giveRedstone() {
         if (agent.getItemCount(1) < 10) {
             agent.setItem(REDSTONE_WIRE, 64, 1)
         }
-
-        placeCheck();
     }
 }
--- t_level1-6.md
+++ t_level1-6.md
@@ -145,6 +145,9 @@ player.onChat("level4", function () {
 player.onChat("level1", function () {
 CodeCosmos.startingPosition();
 })
+loops.forever(function () {
+    CodeCosmos.giveRedstone()
+})
 ```
 ## Herstel het moederbord
 Volg de instructies op het leerplatform om de opdracht op te lossen.
