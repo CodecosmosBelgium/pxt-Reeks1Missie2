3db1467bae0de153833bde3383f4ba9eff1cb05c
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-06 13:17:41 +0100

    Update level_check.ts

--- level_check.ts
+++ level_check.ts
@@ -3,9 +3,9 @@ let correctPositions: Position[] = [];
 
 function placeAndCheck() {
     const detectPostion = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
+    player.say("CP:"+ correctPositions.length);
 
-
-    if (correctPositions.indexOf(detectPostion) === -1 && (blocks.testForBlock(Block.LightGrayConcrete, detectPostion) || blocks.testForBlock(Block.RedstoneBlock, detectPostion))) {
+    if (correctPositions.indexOf(detectPostion) === -1 && (blocks.testForBlock(Block.LightGrayConcrete, detectPostion) || blocks.testForBlock(Block.WhiteConcrete, detectPostion) || blocks.testForBlock(Block.RedstoneBlock, detectPostion))) {
         agent.place(DOWN)
         correctPositions.push(detectPostion);
         player.execute(`execute @c ~ ~ ~ particle rwm:checkmark ~ ~1 ~`)
