7a99fd5767f2f9aaa1a74a988a4d1698c8cb071f
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-06 13:39:57 +0100

    Update custom_functions.ts

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,22 +1,19 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
-    //% block="startingPosition"
-    //% block.loc.nl="zetKlaar"
+    //% block="zetKlaar"
     export function startingPosition() {
         agent.setSlot(1);
         player.execute("function levels/startingPosition");
     }
 
-    //% block="place"
-    //% block.loc.nl="agent beweeg $direction met $amount en plaats"
+    //% block="agent beweeg $direction met $amount en plaats"
     export function place() {
         agent.setItem(REDSTONE_WIRE, 64, 1);
         placeAndCheck();
         
     }
 
-    //% block="agent move $direction by $amount and place"
-    //% block.loc.nl="agent beweeg $direction met $amount en plaats"
+    //% block="agent beweeg $direction met $amount en plaats"
     export function moveAndPlace(direction:FourDirection, amount:number) {
         agent.setItem(REDSTONE_WIRE, 64, 1);
         for(let i = 0; i < amount; i++) {
